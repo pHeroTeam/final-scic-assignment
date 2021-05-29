a72c43a35f5af7f64f3cea55f1d3024b756d9f16
Mohammed Asif <asif.cse473@gmail.com>
2021-05-29 16:08:47 +0600

    advertisement added

--- src/components/homepage/Advertisement/Advertisement.js
+++ src/components/homepage/Advertisement/Advertisement.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import ad001 from '../../../images/fakeAd01.jpg'
+import ad002 from '../../../images/fakeAd02.jpg'
+import ad003 from '../../../images/fakeAd03.jpg'
+import './Advertisement.css'
 
 const Advertisement = () => {
     return (
-        <div>
-            
-        </div>
+        <Container className="mt-5 mb-5">
+            <Row>
+                <Col md={4} lg={4} sm={6} xs={12}>
+                    <img src={ ad001} alt="" className="img-fluid adImage"/>
+                </Col>
+                <Col md={4} lg={4} sm={6} xs={12}>
+                <img src={ ad002} alt="" className="img-fluid adImage"/>
+                </Col>
+                <Col md={4} lg={4} sm={6} xs={12}>
+                <img src={ ad003} alt="" className="img-fluid adImage"/>
+                </Col>
+
+            </Row>
+        </Container>
     );
 };
 
--- src/components/homepage/HomePage.js
+++ src/components/homepage/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Advertisement from './Advertisement/Advertisement';
 import Banner from './Banner/Banner';
 
 const HomePage = () => {
     return (
         <div>
             <Banner></Banner>
+            <Advertisement></Advertisement>
         </div>
     );
 };
