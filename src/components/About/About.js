32d59ea64c9bf07fd52f567009922110612ad70c
Tahsin Nurun <nuruntahsin@gmail.com>
2021-05-29 16:07:33 +0600

    about page content added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div style={{backgroundImage: `url("https://i.ibb.co/wwgnttc/About-Background.jpg")`, }} className="pb-5 h-100">
             <h1 className='text-center py-5 text-white'>OUR STORY OF PRIDE</h1>
-            <div style={{backgroundColor:'#E67E22'}}  className ="container py-5 px-5">
+            <div style={{backgroundColor:'#E67E22', borderRadius:"10px"}}  className ="container py-5 px-5">
                 <div className="row justify-content-between">
                     <div className="col-md-5">
                         <h5 className="text-justify"> The food and culinary traditions prevalent in Bangladesh. Dating far in the past, the cuisine emphasizes fish, vegetables and lentils served with rice. Because of differences in history and Bangladeshi geography, the cuisine is rich in regional variations. While having unique traits, Bangladeshi cuisine is closely related to that of surrounding Bengali and North-East Indian, with rice and fish as traditional favorites. Bangladesh also developed the only multi-course tradition in South Asia. It is known as Bangaliketa styled cuisine</h5> 
