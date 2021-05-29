87d486062fe149e99354ca2076ef7c6ba1a43eba
Mohammed Asif <asif.cse473@gmail.com>
2021-05-29 13:34:22 +0600

    about router added

--- src/App.js
+++ src/App.js
@@ -6,13 +6,19 @@ import {
   Link
 } from "react-router-dom";
 import HomePage from './components/homepage/HomePage';
+import NavBar from './components/NavBar/NavBar';
+import About from './components/About/About';
 
 function App() {
   return (
     <Router>
+      <NavBar></NavBar>
       <Switch>
         <Route path="/" exact>
           <HomePage></HomePage>
+        </Route>
+        <Route path="/about" exact>
+          <About></About>
         </Route>
       </Switch>
     </Router>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
-        <div>
-            
-        </div>
+        <Navbar bg="light" expand="lg">
+  <Container>
+    <Navbar.Brand href="#home">Taste of Bangladesh</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="ml-auto">
+        <NavLink to="/" activeClassName="bg-danger" className="bg-white text-dark p-3 font-weight-bold rounded-lg">   
+            Home
+        </NavLink>
+        <NavLink to="/about" activeClassName="bg-danger text-white" className="p-3 font-weight-bold text-dark rounded-lg">   
+            About
+        </NavLink>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
     );
 };
 
