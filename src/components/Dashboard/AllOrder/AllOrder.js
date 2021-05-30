72eb781c817a2ca20283e8d1092ed84075652b3a
abdulaziz899 <abdulaziz0170934@gmail.com>
2021-05-30 07:46:44 -0400

    admin section update and back end connected success

--- src/components/Dashboard/AllOrder/AllOrder.js
+++ src/components/Dashboard/AllOrder/AllOrder.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import SideBar from '../SideBar/SideBar';
+import React, { useEffect, useState } from 'react';
+import './AllOrder.css'
+import AllOrderDetail from './AllOrderDetail';
 
 const AllOrder = () => {
-        //get url =`http://localhost:4002/showAllFoodAddClientSite`;
+    const [allOrders,setAllOrders]=useState([]);
+    useEffect(()=>{
+        const url =`https://ancient-savannah-78897.herokuapp.com/showAllFoodAddClientSite`;
+        fetch(url)
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data);
+            setAllOrders(data)
+        })
+    },[])
     return (
         <div className='row'>
             <div className="col-md-9">
-                <h1 className='text-capitalize text-center text-success pt-5 pb-3'>User Order:length dynamic</h1>
-                
+                <h1 className='text-capitalize text-center text-success pt-5 pb-3'>User Order:{allOrders.length}</h1>
+                <div className="row">
+                    {
+                        allOrders.map(order=><AllOrderDetail key={order._id} order={order}></AllOrderDetail>)
+                    }
+                </div>
             </div>
         </div>
     );
